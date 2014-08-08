b21c1731d2bb77582a6292b563069456087f49b5
Matthew Govaere <matthew.govaere@gmail.com>
2014-08-08 17:00:08 -0400

    Cleaned up code, removed unnecessary line breaks, tabs to spaces

--- src/Generous/ApiRequestor.php
+++ src/Generous/ApiRequestor.php
@@ -2,141 +2,119 @@
 
 class Generous_ApiRequestor
 {
-	public $apiKey;
+    public $apiKey;
 
-	public function __construct($apiKey = null)
-	{
-		$this->apiKey = $apiKey;
-	}
+    public function __construct($apiKey = null)
+    {
+        $this->apiKey = $apiKey;
+    }
 
-	public function request($method, $endpoints, $params = null)
-	{
-		return $this->requestRaw(strtolower($method), $endpoints, $params);
-	}
+    public function request($method, $endpoints, $params = null)
+    {
+        return $this->requestRaw(strtolower($method), $endpoints, $params);
+    }
 
-	private function requestRaw($method, $endpoints, $params)
-  	{
-  		$user_agent = array
-  		(
-	  		'sdk_version' => Generous::VERSION,
-			'lang' => 'php',
-			'lang_version' => phpversion(),
-			'publisher' => 'generous',
-			'uname' => php_uname()
-		);
+    private function requestRaw($method, $endpoints, $params)
+    {
+        $user_agent = array(
+            'sdk_version' => Generous::VERSION,
+            'lang' => 'php',
+            'lang_version' => phpversion(),
+            'publisher' => 'generous',
+            'uname' => php_uname()
+        );
 
-  		$headers = array
-  		(
-  			'X-Generous-Client-User-Agent: ' . json_encode($user_agent),
-			'User-Agent: Generous/' . Generous::$apiBaseUrlVersion . ' PHP-SDK/' . Generous::VERSION
-		);
+        $headers = array(
+            'X-Generous-Client-User-Agent: ' . json_encode($user_agent),
+            'User-Agent: Generous/' . Generous::$apiBaseUrlVersion . ' PHP-SDK/' . Generous::VERSION
+        );
 
-  		if (isset(Generous::$apiVersion) && Generous::$apiVersion != null)
-  		{
-			$headers[] = 'Generous-Version: ' . Generous::$apiVersion;
-		}
+        if (isset(Generous::$apiVersion) && Generous::$apiVersion != null) {
+            $headers[] = 'Generous-Version: ' . Generous::$apiVersion;
+        }
 
-		if(isset(Generous::$apiKey) && isset(Generous::$apiSecret))
-		{
-			$auth_token = base64_encode(Generous::$apiKey . ':' . Generous::$apiSecret);
-			$headers[] = 'Authorization: Basic ' . $auth_token;
-		}
+        if(isset(Generous::$apiKey) && isset(Generous::$apiSecret)) {
+            $auth_token = base64_encode(Generous::$apiKey . ':' . Generous::$apiSecret);
+            $headers[] = 'Authorization: Basic ' . $auth_token;
+        }
 
-		return $this->curlRequest($method, $endpoints, $params, $headers);
-  	}
+        return $this->curlRequest($method, $endpoints, $params, $headers);
+    }
 
-	private function curlRequest($method, $endpoints, $params, $headers)
-	{
-		$curl 	= curl_init();
-		$opts 	= array();
-		$absURL	= $this->apiUrl($endpoints);
+    private function curlRequest($method, $endpoints, $params, $headers)
+    {
+        $curl   = curl_init();
+        $opts   = array();
+        $absURL = $this->apiUrl($endpoints);
 
-		switch ($method)
-		{
-			case 'get':
+        switch ($method) {
+            case 'get':
+                if (count($params) > 0) {
+                    $encoded = self::encode($params);
+                    $absURL = "$absURL?$encoded";
+                }
+            break;
 
-				if (count($params) > 0) 
-				{
-					$encoded = self::encode($params);
-					$absURL = "$absURL?$encoded";
-				}
+            case 'post':
+                $opts[CURLOPT_POST] = true;
+                $opts[CURLOPT_POSTFIELDS] = self::encode($params);
+            break;
 
-			break;
+            case 'put':
+                $opts[CURLOPT_CUSTOMREQUEST] = 'PUT';
+                $opts[CURLOPT_POSTFIELDS] = self::encode($params);
+            break;
 
-			case 'post':
+            case 'delete':
+                $opts[CURLOPT_CUSTOMREQUEST] = 'delete';
+                $opts[CURLOPT_POSTFIELDS] = http_build_query($params);
+            break;
+        }
 
-				$opts[CURLOPT_POST] = true;
-				$opts[CURLOPT_POSTFIELDS] = self::encode($params);
+        $opts[CURLOPT_URL] = $absURL;
+        $opts[CURLOPT_CONNECTTIMEOUT] = 30;
+        $opts[CURLOPT_TIMEOUT] = 80;
+        $opts[CURLOPT_RETURNTRANSFER] = true;
+        $opts[CURLOPT_HTTPHEADER] = $headers;
+        $opts[CURLOPT_SSL_VERIFYPEER] = 0;
 
-			break;
+        curl_setopt_array($curl, $opts);
 
-			case 'put':
+        $response = curl_exec($curl);
 
-				$opts[CURLOPT_CUSTOMREQUEST] = 'PUT';
-				$opts[CURLOPT_POSTFIELDS] = self::encode($params);
+        curl_close($curl);
 
-			break;
+        return json_decode($response, true);
+    }
 
-			case 'delete':
+    private static function apiUrl($endpoints = '')
+    {
+        $apiBaseUrl = Generous::$apiBaseUrl;
 
-				$opts[CURLOPT_CUSTOMREQUEST] = 'delete';
-				$opts[CURLOPT_POSTFIELDS] = http_build_query($params);
+        return $apiBaseUrl . $endpoints;
+    }
 
-			break;
-		}
+    public static function encode($arr, $prefix = null)
+    {
+        if (!is_array($arr)) return $arr;
+        $r = array();
 
-		$opts[CURLOPT_URL] = $absURL;
-		$opts[CURLOPT_CONNECTTIMEOUT] = 30;
-		$opts[CURLOPT_TIMEOUT] = 80;
-		$opts[CURLOPT_RETURNTRANSFER] = true;
-		$opts[CURLOPT_HTTPHEADER] = $headers;
-		$opts[CURLOPT_SSL_VERIFYPEER] = 0;
+        foreach ($arr as $k => $v) {
+            if (is_null($v)) continue;
 
-		curl_setopt_array($curl, $opts);
+            if ($prefix && $k && !is_int($k)) {
+                $k = $prefix."[".$k."]";
+            } else if ($prefix) {
+                $k = $prefix."[]";
+            }
 
-		$response = curl_exec($curl);
+            if (is_array($v)) {
+                $r[] = self::encode($v, $k, true);
+            } else {
+                $r[] = urlencode($k)."=".urlencode($v);
+            }
+        }
 
-		curl_close($curl);
-
-		return json_decode($response, true);
-	}
-
-	private static function apiUrl($endpoints = '')
-	{
-		$apiBaseUrl = Generous::$apiBaseUrl;
-
-		return $apiBaseUrl . $endpoints;
-	}
-
-	public static function encode($arr, $prefix = null)
-	{
-		if (!is_array($arr)) return $arr;
-
-		$r = array();
-
-		foreach ($arr as $k => $v) 
-		{
-			if (is_null($v)) continue;
-
-			if ($prefix && $k && !is_int($k))
-			{
-				$k = $prefix."[".$k."]";
-			}
-			else if ($prefix)
-			{
-				$k = $prefix."[]";
-			}
-
-			if (is_array($v)) 
-			{
-				$r[] = self::encode($v, $k, true);
-			} 
-			else 
-			{
-				$r[] = urlencode($k)."=".urlencode($v);
-			}
-		}
-
-		return implode("&", $r);
-	}
+        return implode("&", $r);
+    }
 }
--- src/Generous/Generous.php
+++ src/Generous/Generous.php
@@ -2,50 +2,50 @@
 
 abstract class Generous
 {
-	public static $apiKey;
-	public static $apiSecret;
+    public static $apiKey;
+    public static $apiSecret;
 
-	public static $apiVersion;
+    public static $apiVersion;
 
-	public static $apiBaseUrl = 'http://api.genero.us';
-	public static $apiBaseUrlVersion = 'v1';
+    public static $apiBaseUrl = 'http://api.genero.us';
+    public static $apiBaseUrlVersion = 'v0';
 
-	const VERSION = '0.1.1';
+    const VERSION = '0.1.1';
 
-	public static function getApiKey()
-	{
-		return self::$apiKey;
-	}
+    public static function getApiKey()
+    {
+        return self::$apiKey;
+    }
 
-	public static function setApiKey($apiKey, $apiSecret = null)
-	{
-		self::$apiKey = $apiKey;
+    public static function setApiKey($apiKey, $apiSecret = null)
+    {
+        self::$apiKey = $apiKey;
 
-		if($apiKey != null)
-		{
-			self::$apiSecret = $apiSecret;
-		}
+        if($apiKey != null)
+        {
+            self::$apiSecret = $apiSecret;
+        }
 
-		self::setBaseUrl(self::$apiBaseUrl, self::$apiBaseUrlVersion);
-	}
+        self::setBaseUrl(self::$apiBaseUrl, self::$apiBaseUrlVersion);
+    }
 
-	public static function setApiVersion($apiVersion)
-	{
-		self::$apiVersion = $apiVersion;
-	}
+    public static function setApiVersion($apiVersion)
+    {
+        self::$apiVersion = $apiVersion;
+    }
 
-	public static function setBaseUrl($apiBaseUrl = null, $apiBaseUrlVersion = null)
-	{
-		if($apiBaseUrl == null) $apiBaseUrl = self::$apiBaseUrl;
-		if($apiBaseUrlVersion != null) self::$apiBaseUrlVersion = $apiBaseUrlVersion;
+    public static function setBaseUrl($apiBaseUrl = null, $apiBaseUrlVersion = null)
+    {
+        if($apiBaseUrl == null) $apiBaseUrl = self::$apiBaseUrl;
+        if($apiBaseUrlVersion != null) self::$apiBaseUrlVersion = $apiBaseUrlVersion;
 
-		self::$apiBaseUrl = $apiBaseUrl . '/' . self::$apiBaseUrlVersion . '/';
-	}
+        self::$apiBaseUrl = $apiBaseUrl . '/' . self::$apiBaseUrlVersion . '/';
+    }
 
-	public static function customRequest($method, $endpoints, $params = null)
-	{
-		$requestor = new Generous_ApiRequestor(self::$apiKey);
+    public static function customRequest($method, $endpoints, $params = null)
+    {
+        $requestor = new Generous_ApiRequestor(self::$apiKey);
 
-		return $requestor->request($method, $endpoints, $params);
-	}
+        return $requestor->request($method, $endpoints, $params);
+    }
 }
